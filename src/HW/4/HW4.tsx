5d4226ea276671129cd0ec1eb07dc188d6043577
SergeyStarchenko <pdxserg@gmail.com>
2024-04-28 12:19:22 -0700

    hom work 4 finished

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -42,8 +42,8 @@ export const HW4 = () => {
         {texts.map((el, index) => {
           return (
             <li key={index} id={`hw04-task-${index}`}
-                className={index%2  ? s.chetNechet : ''}
-
+                className={index%2  ?'': s.chetNechet }
+                // className={s.chetNechet}
             >
               {el}
             </li>
